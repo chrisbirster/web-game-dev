518d58f2052097469979bda6cbf2f289b7647117
Chris Birster <christopher.birster@gmail.com>
2023-04-11 14:00:20 -0400

    build configs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -9,7 +9,7 @@
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  transpilePackages: ["@acme/api", "@acme/db"],
+  transpilePackages: ["@wgd/api", "@wgd/db"],
   // We already do linting on GH actions
   eslint: {
     ignoreDuringBuilds: !!process.env.CI,
---
+++ apps/web/postcss.config.cjs
@@ -0,0 +1,1 @@
+module.exports = require("@wgd/tailwind-config/postcss");
